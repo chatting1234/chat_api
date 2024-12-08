1ea68c2b61b3b8e45e4471b9930a88846ca73535
Janghyunsung2 <tlswlfk@gmail.com>
2024-12-08 20:13:44 +0900

    cc

---
+++ src/main/java/com/nhnacademy/chat_api/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.chat_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "chat_api";
+    }
+}
